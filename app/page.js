c6758e39ff4beb995b97872a43403d6de5978c9e
arafatyamin <mdh631987@gmail.com>
2023-04-23 23:15:02 +0600

    add custom components folder

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
 
-
-
 export default function Home() {
   return (
     <main>
       <div>
-        <h1>hello world</h1>
-        <h1>hello world 2</h1>
-        <h2>h</h2>
+        
       </div>
     </main>
   )
