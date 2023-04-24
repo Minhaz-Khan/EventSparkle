a6283abdffebbc012ea2fe9d0caaaf6f9de8888e
arafatyamin <mdh631987@gmail.com>
2023-04-24 20:02:01 +0600

    add header,footer and page str

--- Components/shared/Footer.js
+++ Components/shared/Footer.js
@@ -1,8 +1,69 @@
+import Link from "next/link";
+
 const Footer = () => {
     return (
-        <div>
-            <h1>this is footer</h1>
+        <footer className="py-5 bg-[#001C38] text-white">
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+          <div>
+            <h3 className="text-2xl mb-3">EventSparkle</h3>
+            <p>Hi, I’m Samantha, a wedding planner, and designer who loves life in the wonderful New York. The nestam acuam nec odio the elementum.</p>
+          </div>
+          <div>
+            <h3 className="mb-3 text-lg">Explore</h3>
+            <ul>
+              <li className="my-2">
+                <Link href={"#"} className="hover:text-slate-500">
+                  About
+                </Link>
+              </li>
+              <li className="my-2">
+                <Link href={"#"} className="hover:text-slate-500">
+                  Services
+                </Link>
+              </li>
+              <li className="my-2">
+                <Link href={"#"} className="hover:text-slate-500">
+                  Portfolio
+                </Link>
+              </li>
+              <li className="my-2">
+                <Link href={"#"} className="hover:text-slate-500">
+                  Gallery
+                </Link>
+              </li>
+              <li className="my-2">
+                <Link href={"#"} className="hover:text-slate-500">
+                  Blog
+                </Link>
+              </li>
+              <li className="my-2">
+                <Link href={"#"} className="hover:text-slate-500">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="mb-3 text-lg">Get In Touch</h3>
+            <address>
+                <p>1010 Broadway NY, New York 10001
+United States of America</p>
+              <a className="mb-2 inline-block" href="tel:+8801789000000">
+                Mob: +8801789-000000
+              </a>
+              <br />
+              <a href="mailto:EventSparkle@gmail.com" className="border-b">EventSparkle@gmail.com</a>
+            </address>
+          </div>
         </div>
+        <div className="mt-3">
+          <p className="p-2 text-center">
+            Copyright © 2023, EventSparkle All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
     );
 };
 
--- Components/shared/Navbar.js
+++ Components/shared/Navbar.js
@@ -1,8 +1,61 @@
+'use client';
+import Link from "next/link";
+import { useState } from "react";
+import { GoThreeBars } from "react-icons/go";
+import { GrFormClose } from "react-icons/gr";
+
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(true); 
+    console.log(isOpen)
     return (
-        <div>
-            <h1>this is navbar</h1>
-        </div>
+      <header className="py-5 bg-white">
+      <div className="container">
+        <nav className="flex justify-between items-center">
+          <Link href="/">
+          <h1 className="text-2xl">EventSparkle</h1>
+          </Link>
+          <ul
+            className={`${
+              isOpen
+                ? "fixed top-0 left-0 right-[50%] h-full flex flex-col justify-center items-center bg-white"
+                : "hidden md:relative md:block "}`}
+          >
+            <li className="inline-block mx-3">
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li className="inline-block mx-3">
+              <Link href={"about"}>About</Link>
+            </li>
+            <li className="inline-block mx-3">
+              <Link href={"services"}>Services</Link>
+            </li>
+            <li className="inline-block mx-3">
+              <Link href={"portfolio"}>Portfolio</Link>
+            </li>
+            <li className="inline-block mx-3">
+              <Link href={"gallery"}>Gallery</Link>
+            </li>
+            <li className="inline-block mx-3">
+              <Link href={"contact-us"}>Contact Us</Link>
+            </li>
+            <span
+              className={`${isOpen ? "absolute top-6 right-6" : "md:hidden"}`}
+            >
+              <GrFormClose size={30} onClick={() => setIsOpen(false)} />
+            </span>
+          </ul>
+
+          <span className="block md:hidden">
+            <GoThreeBars
+              size={25}
+              className="cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          </span>
+        </nav>
+      </div>
+    </header>
     );
 };
 
